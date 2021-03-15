08d2f466af230d0f8de12ec5d8f77321da631ed9
M. Cullen McClellan <cullenmcclellan@gmail.com>
2021-03-15 09:48:37 -0400

    GET posts complete, index.js setup for listening, post-router and server structure setup

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,19 @@
 // implement your posts router here
+const express = require('express');
+
+const router = express.Router();
+const Posts = require('./posts-model');
+
+
+router.get('/', (req, res) => {
+    Posts.find()
+        .then(blogs => {
+            res.status(200).json(blogs);
+        })
+        .catch(err => res.status(500).json({
+            message : 'Error retrieving Blog Posts',
+            error : err
+        }))
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,11 @@
 // implement your server here
+const express = require('express');
+const PostRouter = require('./posts/posts-router')
+const server = express();
+server.use(express.json())
+
+server.use('/api/posts', PostRouter)
 // require your posts router and connect it here
+
+
+module.exports = server;
