4adc10cabeb75231a3a5fd6bf5d0b39568b8c894
M. Cullen McClellan <cullenmcclellan@gmail.com>
2021-03-15 10:40:23 -0400

    GET comments complete, MVP met

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -94,4 +94,23 @@ router.delete('/:id', async (req, res) => {
     })
 })
 
+router.get('/:id/comments', async (req, res) => {
+    const { id } = req.params;
+
+    await Posts.findPostComments(id)
+    .then(blog => {
+        if(!blog){
+            res.status(404).json(
+                { message: "The post with the specified ID does not exist" })
+        }
+        else{
+            res.status(200).json(blog);
+        }     
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ message: "The comments information could not be retrieved" })
+    })
+})
+
 module.exports = router;
