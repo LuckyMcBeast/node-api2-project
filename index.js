19ee7a364e449e7e062aecc7bb60de723b17c9ef
M. Cullen McClellan <cullenmcclellan@gmail.com>
2021-03-18 23:20:23 -0400

    dotenv installed, .env added on local, process.env.PORT added, .env on .gitignore

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 // require your server and launch it here
 const server = require('./api/server.js');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Server Running of ${PORT}`)
 })
