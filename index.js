e0fe4081c698e1251219d45039e13b8ae2d3446d
M. Cullen McClellan <cullenmcclellan@gmail.com>
2021-03-18 23:28:44 -0400

    Minor bug fix?

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 // require your server and launch it here
 const server = require('./api/server.js');
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-    console.log(`Server Running of ${PORT}`)
+const port = process.env.PORT;
+
+server.listen(port, () => {
+    console.log(`Server Running of ${port}`)
 })
